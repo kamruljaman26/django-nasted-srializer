efb0cfd202e7be5bd505e398f417255dd94c3d2e
kamruljaman26 <kamrul.jaman26@gmail.com>
2020-03-06 00:32:27 +0600

    commit

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/kamruljaman/Desktop/GitHub/Database_Relationship/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
